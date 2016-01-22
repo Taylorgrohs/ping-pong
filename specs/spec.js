46ca418a2c919dbabf9c30dcd8a0c401986f5d94
Taylor Grohs <taylorgrohs@gmail.com>
2016-01-22 08:15:39 -0800

    Add failing test 2

--- specs/spec.js
+++ specs/spec.js
@@ -1,5 +1,8 @@
 describe("pingPong", function(){
-  it("will count to the number that it was given.", function(){
+  it("will count to the number that it was given and return each number.", function(){
     expect(pingPong(5)).to.eql([1, 2, 3, 4, 5]);
+  })
+  it("will print out 'ping' when the number is divisible by 3", function(){
+    expect(3).to.equal("ping");
   })
 })
