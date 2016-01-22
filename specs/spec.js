f836942001b8f8ed4bf98a006ec681676b882a0a
Taylor Grohs <taylorgrohs@gmail.com>
2016-01-22 08:42:30 -0800

    Add failing test 4

--- specs/spec.js
+++ specs/spec.js
@@ -8,4 +8,7 @@ describe("pingPong", function(number){
   it('will print out "pong" when the number is divisible by 5', function(){
     expect(pingPong(5)).to.eql([1, 2, "ping", 4, "pong"])
   })
+  it('will print out "pingpong" when the number is divisible by 15', function(){
+    expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"])
+  })
 })
