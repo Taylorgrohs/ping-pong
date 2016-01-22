3f382415229adecb06a3e69cb2a9760efbc8126d
Taylor Grohs <taylorgrohs@gmail.com>
2016-01-22 08:48:59 -0800

    pass fourth test

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 var pingPong = function(number) {
     var array = []
     for (var i = 1; i <= number; i++) {
-      if (i % 3 === 0) {
-        array.push("ping");
+      if ( i % 15 === 0) {
+        array.push("pingpong");
+        console.log(array);
       } else if ( i % 5 === 0) {
         array.push("pong");
+        console.log(array);
+      } else if ( i % 3 === 0) {
+        array.push("ping");
+        console.log(array);
       } else {
         array.push(i);
+        console.log(array);
     }
   }
   return array;
